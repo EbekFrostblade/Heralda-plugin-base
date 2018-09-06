e87a930fe0a1563659871ff62277d09c2a15c993
Brad Jones <bradster.jones@gmail.com>
2018-09-06 15:36:39 -0500

    Allow the module to define custom default configs
    These configs can be over-written in Heralda's `_config.json` file, so
    servers can customize their plugins.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const Responder = require("./classes/responder.js");
 
+let defaultConfig = {};
+
+try {
+  defaultConfig = require.resolve('./config.json');
+}
+catch (err) {
+  console.log('No default configuration found.')
+}
+
 class HeraldaPlugin {
   constructor(client, config) {
     this.client = client;
     this.responder = new Responder(client);
-    this.config = config;
+    this.config = mergeConfigs(config);
 
     this.init(client, config);
   }
+}
+
+function mergeConfigs(clientConfig) {
+  let finalConfig = defaultConfig;
+  for (let key in clientConfig) {
+    finalConfig[key] = clientConfig[key];
+  }
+
+  return finalConfig;
 }
 
 HeraldaPlugin.Responder = Responder;
