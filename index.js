5cd595f3e119702436895083e89ee02290d28282
Brad Jones <bradster.jones@gmail.com>
2018-09-07 10:55:08 -0500

    Add recursive configuration options.
    After realizing that I could have nested configs, I decided to make this
    function recursive. It will now loop through all flags in the client
    config, to make sure it gets all of the nested stuff, and won't nuke any
    of the default options.

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ HeraldaPlugin.Responder = Responder;
 HeraldaPlugin.mergeConfigs = (clientConfig, defaultConfig) => {
   let finalConfig = defaultConfig;
   for (let key in clientConfig) {
-    finalConfig[key] = clientConfig[key];
+    if (finalConfig[key] && typeof(finalConfig[key]) === 'object' && !Array.isArray(finalConfig[key])) {
+      finalConfig[key] = HeraldaPlugin.mergeConfigs(clientConfig[key], finalConfig[key]);
+    }
+    else {
+      finalConfig[key] = clientConfig[key];
+    }
   }
 
   return finalConfig;
